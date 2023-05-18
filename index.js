3b117004de5757ea1534cd1381512f0445d56ee8
HK-MBURU <96179053+HK-MBURU@users.noreply.github.com>
2023-05-18 11:07:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const items=document.querySelector(".items")
+const budget=document.createElement("div");
+const title=document.createElement("div");
+title.innerText="School"
+const category=document.createElement("div");
+category.innerText="Expense"
+const amount=document.createElement("div");
+amount.innerText="50000$"
+const btn_delete=document.createElement("button");
+btn_delete.innerText="Delete"
+
+
+items.appendChild(budget);
+
